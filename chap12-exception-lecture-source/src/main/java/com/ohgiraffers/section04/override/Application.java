2127ab769546697ecb2281033a877e4c97975d84
LAPTOP-FCN2RE9I\Playdata <wkdalstjr94@gmail.com>
2024-01-24 11:48:52 +0900

    chap12. 예외처리

---
+++ chap12-exception-lecture-source/src/main/java/com/ohgiraffers/section04/override/Application.java
@@ -0,0 +1,18 @@
+package com.ohgiraffers.section04.override;
+
+import java.io.IOException;
+
+public class Application {
+    public static void main(String[] args) {
+        SuperClass sc = new SubClass();
+
+        try {
+            sc.method();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+
+        }
+
+    }
+}
