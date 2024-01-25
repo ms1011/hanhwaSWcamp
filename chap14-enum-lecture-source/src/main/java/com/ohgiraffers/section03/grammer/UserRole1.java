ac5e142547a7bb1a4399e078425ce27b222cdd87
LAPTOP-FCN2RE9I\Playdata <wkdalstjr94@gmail.com>
2024-01-25 12:25:44 +0900

    chap14. enum

---
+++ chap14-enum-lecture-source/src/main/java/com/ohgiraffers/section03/grammer/UserRole1.java
@@ -0,0 +1,20 @@
+package com.ohgiraffers.section03.grammer;
+
+public enum UserRole1 {
+    GUEST,
+    CONSUMER,
+    PRODUCER,
+    ADMIN;
+
+    /* 필기.
+     *  생성자를 가질 수 있다. 하지만, 생성자는 외부에서 사용하지 못하도록 (싱글톤 패턴) default 또는 private 접근 제어자만 사용 가능하다
+     *  (enum 타입은 고정된 상수들의 집합이며 한번 할당된 객체는 변하지 않는다)
+    * */
+    UserRole1() {
+        System.out.println("기본 생성자 호출 확인");
+    }
+
+    public String getNameToLowerCase() {
+        return this.name().toLowerCase();
+    }
+}
